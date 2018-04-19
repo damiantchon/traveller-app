337ba60b5402344779f2e4fe0ebc65069dc8b91e
Damian Tchoń <damtch@gmail.com>
2018-04-19 15:08:46 +0200

    Initial Commit

---
+++ app/src/main/java/tim/project/travellerapp/MainActivity.java
@@ -0,0 +1,22 @@
+package tim.project.travellerapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onLoginClick(View view) {
+
+    }
+
+    public void onSignupClick(View view) {
+
+    }
+}
